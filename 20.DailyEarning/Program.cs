025aa75c1caaa87be91648336303da06d19a16d1
Someone Else <medical_boy_sf@yahoo.com>
2018-04-17 02:44:52 +0300

    20. Daily Earning Task

---
+++ 20.DailyEarning/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _20.DailyEarning
+{
+    class Program
+    {
+        static void Main()
+        {
+            double workingDays = double.Parse(Console.ReadLine());
+            double earnedMoneyPerDay = double.Parse(Console.ReadLine());
+            double currencyUSDtoBGN = double.Parse(Console.ReadLine());
+            double yearlyEarning = workingDays * earnedMoneyPerDay * 12 + workingDays * earnedMoneyPerDay * 2.5;
+            double TotalWithtaxesBGN = yearlyEarning -  yearlyEarning * 0.25;
+            double TotalWithtaxesUSD = TotalWithtaxesBGN * currencyUSDtoBGN;
+            double meanEarningPerDay = TotalWithtaxesUSD / 365;
+            Console.WriteLine($"{meanEarningPerDay:F2}");
+
+        }
+    }
+}
