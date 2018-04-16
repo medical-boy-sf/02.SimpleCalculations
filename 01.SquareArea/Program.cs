db696f84c291cfa2fea66f87294b9d2393ef3dab
Someone Else <medical_boy_sf@yahoo.com>
2018-04-16 10:24:52 +0300

    Tasks 01-02

---
+++ 01.SquareArea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01.SquareArea
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine());
+            double area = a * a;
+            Console.WriteLine($"Square = {area}");
+        }
+    }
+}
---
+++ 02.InchesToCentimeters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02.InchesToCentimeters
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("inches = ");
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = inches * 2.54;
+            Console.WriteLine($"centimeters = {centimeters}");
+        }
+    }
+}
