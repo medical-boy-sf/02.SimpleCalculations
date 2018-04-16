b7dd1ffea2cb723e79095e70b6eaa81cb16daff1
Someone Else <medical_boy_sf@yahoo.com>
2018-04-16 23:49:11 +0300

    Training Labs

---
+++ 16.TrainingLabs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _16.TrainingLabs
+{
+    class Program
+    {
+        static void Main()
+        {
+            double h = double.Parse(Console.ReadLine()) * 100;
+            double w = double.Parse(Console.ReadLine()) * 100;
+            double rows = Math.Truncate(h / 120);
+            double desks = Math.Truncate((w - 100) / 70);
+            double seats = rows * desks - 3;
+            Console.WriteLine(seats);
+
+        }
+    }
+}
