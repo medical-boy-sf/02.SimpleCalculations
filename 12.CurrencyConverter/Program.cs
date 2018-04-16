6c6c665586245d689acb29fad44f9fa3930b6ae1
Someone Else <medical_boy_sf@yahoo.com>
2018-04-16 14:12:04 +0300

    Part of task 12

---
+++ 12.CurrencyConverter/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _12.CurrencyConverter
+{
+    class Program
+    {
+        static void Main()
+        {
+            double val = double.Parse(Console.ReadLine());
+            string currency = Console.ReadLine().ToUpper();
+            string convertedCurrency = Console.ReadLine().ToUpper();
+            double total = 0;
+            double bgn = 1;
+            double usd = 1.79549;
+            double eur = 1.95583;
+            double gbp = 2.53405;
+
+            if (currency == "BGN")
+            {
+                switch (convertedCurrency)
+                {
+                    case "USD":
+                        total = val * bgn / usd;
+                        break;
+
+                    case "EUR":
+                        total = val * bgn / eur;
+                        break;
+
+                    case "GBP":
+                        total = val * bgn / gbp;
+                        break;
+                }
+            }
+
+            else if (currency == "USD")
+            {
+                switch (convertedCurrency)
+                {
+                    case "BGN":
+                        total = val * usd / bgn;
+                        break;
+
+                    case "EUR":
+                        total = val * usd / eur;
+                        break;
+
+                    case "GBP":
+                        total = val * usd / gbp;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{total:F2} {convertedCurrency}");
+        }
+    }
+}
