b5a6231fa5e76455a80698952d186b5b164ac01a
Someone Else <medical_boy_sf@yahoo.com>
2018-04-17 00:10:19 +0300

    17.Grocery Market Task

---
+++ 17.GroceryMarket/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _17.GroceryMarket
+{
+    class Program
+    {
+        static void Main()
+        {
+            double priceVegetables = double.Parse(Console.ReadLine());
+            double priceFruits = double.Parse(Console.ReadLine());
+            double quantityVegetables = double.Parse(Console.ReadLine());
+            double quantityFruits = double.Parse(Console.ReadLine());
+
+            double totalPriceEur = (priceFruits * quantityFruits + priceVegetables * quantityVegetables) / 1.94;
+
+            Console.WriteLine($"{Math.Round(totalPriceEur, 2):F2}");
+
+        }
+    }
+}
